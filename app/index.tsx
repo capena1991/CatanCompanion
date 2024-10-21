327d6b83b18c2d496ca18b179c6b80a9fc627670
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2024-10-21 13:26:45 +0200

    Add basic menu layout with header img

--- app/index.tsx
+++ app/index.tsx
@@ -2,40 +2,27 @@ import { Image, StyleSheet } from "react-native";
 
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 
 export default function Index() {
   return (
-    <ParallaxScrollView
-      headerImage={
-        <Image
-          source={require("@/assets/images/header-img.jpg")}
-          style={styles.headerImg}
-        />
-      }
-    >
-      <ThemedView>
-        <ThemedText>Edit app/index.tsx to edit this screen.</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ThemedView style={styles.menuScreen}>
+      <Image
+        source={require("@/assets/images/header-img.jpg")}
+        style={styles.headerImg}
+      />
+      <ThemedText>Edit app/index.tsx to edit this screen.</ThemedText>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  // titleContainer: {
-  //   flexDirection: 'row',
-  //   alignItems: 'center',
-  //   gap: 8,
-  // },
-  // stepContainer: {
-  //   gap: 8,
-  //   marginBottom: 8,
-  // },
-  headerImg: {
+  menuScreen: {
+    display: "flex",
+    flexDirection: "column",
     height: "100%",
+  },
+  headerImg: {
+    height: "33%",
     width: "100%",
-    bottom: 0,
-    left: 0,
-    position: "absolute",
   },
 });
