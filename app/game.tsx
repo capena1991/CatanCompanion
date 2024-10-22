fa2eca4c3685ad59f2936a1676ea1d92cf08a6f4
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2024-10-22 15:16:27 +0200

    Show basic game stats

--- app/game.tsx
+++ app/game.tsx
@@ -2,39 +2,100 @@ import { View, StyleSheet } from "react-native";
 import { NumberStats } from "@/components/NumberStats";
 import { ThemedView } from "@/components/ThemedView";
 import { useMemo, useState } from "react";
+import { useGameStats } from "@/hooks/useGameStats";
+import { ThemedText } from "@/components/ThemedText";
+
+const formatStat = (x: number) => (isNaN(x) ? "—" : Math.round(x * 10) / 10);
 
 export default function Game({}) {
   const [diceThrows, setDiceThrows] = useState<number[]>([]);
 
-  const normalizedFreqs = useMemo(() => {
-    const freqs = new Array<number>(11).fill(0);
-    diceThrows.forEach((n) => {
-      freqs[n - 2] += 1;
-    });
-    const maxFreq = Math.max(...freqs);
-    return freqs.map((f) => f / maxFreq);
-  }, [diceThrows]);
+  const { actual, expected } = useGameStats(diceThrows);
 
   return (
     <ThemedView style={styles.statsContainer}>
-      {new Array(11).fill(null).map((_, i) => (
-        <NumberStats
-          key={i}
-          number={i + 2}
-          relativeFrequency={normalizedFreqs[i]}
-          onAdd={() => setDiceThrows([...diceThrows, i + 2])}
+      <View style={styles.numberStatsContainer}>
+        {new Array(11).fill(null).map((_, i) => (
+          <NumberStats
+            key={i}
+            number={i + 2}
+            relativeFrequency={actual.normalizedFreqs[i]}
+            onAdd={() => setDiceThrows([...diceThrows, i + 2])}
+          />
+        ))}
+      </View>
+      <View style={styles.summaryStatsContainer}>
+        <SingleSummaryStat
+          name="Mean"
+          expected={expected.mean}
+          actual={actual.mean}
         />
-      ))}
+        <SingleSummaryStat
+          name="STD"
+          expected={expected.standardDeviation}
+          actual={actual.standardDeviation}
+        />
+        <SingleSummaryStat
+          name="Variance"
+          expected={expected.variance}
+          actual={actual.variance}
+        />
+      </View>
     </ThemedView>
+  );
+}
+
+interface SingleSummaryStatProps {
+  name: string;
+  expected: number;
+  actual: number;
+}
+function SingleSummaryStat({ name, expected, actual }: SingleSummaryStatProps) {
+  return (
+    <View style={styles.singleSummaryStat}>
+      <ThemedText style={[styles.statText, styles.statNameText]}>
+        {name}
+      </ThemedText>
+      <ThemedText style={[styles.statText, styles.statExpectedText]}>
+        {formatStat(expected)}
+      </ThemedText>
+      <ThemedText style={[styles.statText, styles.statActualText]}>
+        {formatStat(actual)}
+      </ThemedText>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   statsContainer: {
+    height: "100%",
+    padding: 10,
+    gap: 20,
+  },
+  numberStatsContainer: {
     display: "flex",
     flexDirection: "column",
-    height: "100%",
     gap: 10,
-    padding: 10,
+  },
+  summaryStatsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  singleSummaryStat: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  statText: {
+    fontSize: 20,
+  },
+  statNameText: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  statExpectedText: {},
+  statActualText: {
+    color: "rgba(252, 214, 87, 0.6)",
   },
 });
